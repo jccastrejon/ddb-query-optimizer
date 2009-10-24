48ef3d1cf1c73b60cca107398f0c4f80d3e2876c
jccastrejon <jccastrejon@1581e1ec-bf6e-11de-bbac-836dcc338a7a>
2009-10-24 03:11:45 +0000

    Added conditions support

--- DistributedQueryOptimizer/sql/testQueries.sql
+++ DistributedQueryOptimizer/sql/testQueries.sql
@@ -5,3 +5,7 @@ SELECT nombre, nombre2 FROM tabla;
 SELECT tabla.*, nombre FROM tabla;
 SELECT tabla.*, nombre FROM tabla, tabla2;
 SELECT tabla.*, nombre FROM (SELECT * FROM tabla);
+SELECT * FROM tabla WHERE atributo=1;
+SELECT * FROM tabla WHERE atributo=1 and atributo2=2;
+SELECT * FROM tabla WHERE atributo2 in (1,2);
+SELECT * FROM tabla WHERE atributo2 in (select * from tabla);
