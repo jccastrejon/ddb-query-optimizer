c93546aaac539035f89e478009167d319dcc38f7
jccastrejon <jccastrejon@1581e1ec-bf6e-11de-bbac-836dcc338a7a>
2009-10-28 20:12:36 +0000

    Non-static Parser

--- DistributedQueryOptimizer/tst/mx/itesm/ddb/parser/SqlParserTest.java
+++ DistributedQueryOptimizer/tst/mx/itesm/ddb/parser/SqlParserTest.java
@@ -3,12 +3,14 @@ package mx.itesm.ddb.parser;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
 
 import junit.framework.TestCase;
-import mx.itesm.ddb.util.QueryData;
+import mx.itesm.ddb.service.OptimizerManager;
+import mx.itesm.ddb.service.Query;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.helpers.Loader;
 
 /**
  * SQL Parser tests.
@@ -23,6 +25,11 @@ public class SqlParserTest extends TestCase {
      */
     Logger logger = Logger.getLogger(SqlParserTest.class.getName());
 
+    @Override
+    public void setUp() {
+	PropertyConfigurator.configure(Loader.getResource("test-log4j.properties"));
+    }
+
     /**
      * Execute the parser using each query in the '/sql/testQueries.sql' file
      * 
@@ -30,39 +37,32 @@ public class SqlParserTest extends TestCase {
      *             If the '/sql/testQueries.sql' cannot be properly read.
      */
     public void testQuery() throws IOException {
+	Query query;
 	int queryCount;
-	String query;
-	SqlParser parser;
+	String testQuery;
 	boolean correctQueries;
 	BufferedReader testReader;
-	QueryData queryStatement;
+	OptimizerManager optimizerManager = new OptimizerManager();
 
+	// Load test file
 	testReader = new BufferedReader(new FileReader("./sql/testQueries.sql"));
-	parser = null;
 	queryCount = 0;
 	correctQueries = true;
 
-	// Try to parse each query on the file
-	while ((query = testReader.readLine()) != null) {
+	// Try to parse each query in the file
+	while ((testQuery = testReader.readLine()) != null) {
+	    query = optimizerManager.createQuery(testQuery);
 	    queryCount++;
 
-	    // Considering a static parser
-	    if (parser == null) {
-		parser = new SqlParser(new StringReader(query));
-	    } else {
-		SqlParser.ReInit(new StringReader(query));
-	    }
-
-	    try {
-		queryStatement = SqlParser.QueryStatement();
-		logger.info("\nQuery #" + queryCount + " correctly parsed:\n" + queryStatement);
-	    } catch (Exception e) {
-		logger.error("Problems in query #" + queryCount, e);
+	    // It should start with the Projection symbol
+	    if (!query.getRelationalAlgebra().startsWith("&#0928;")) {
 		correctQueries = false;
+		logger.error("Problems transforming query #" + queryCount + " '" + testQuery
+			+ " into relational algebra: " + query.getRelationalAlgebra());
 	    }
 	}
 
-	assertEquals(correctQueries, true);
 	logger.info("Correctly parsed queries: " + queryCount);
+	assertEquals(correctQueries, true);
     }
 }
