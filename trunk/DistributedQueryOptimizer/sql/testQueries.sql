078ee1715651759377074691bbca922dbd6eedea
jccastrejon <jccastrejon@1581e1ec-bf6e-11de-bbac-836dcc338a7a>
2009-10-29 18:52:39 +0000

    Add support for table.attribute in SELECT and WHERE clauses

--- trunk/DistributedQueryOptimizer/sql/testQueries.sql
+++ trunk/DistributedQueryOptimizer/sql/testQueries.sql
@@ -9,3 +9,6 @@ SELECT * FROM tabla WHERE atributo=1;
 SELECT * FROM tabla WHERE atributo=1 and atributo2=2;
 SELECT * FROM tabla WHERE atributo2 in (1,2);
 SELECT * FROM tabla WHERE atributo2 in (select * from tabla);
+select * from tabla1, tabla2 where tabla1.atributo = tabla2.atributo;
+select tabla.atributo from tabla;
+select tabla.atributo from tabla, tabla2 where tabla1.atributo = tabla2.atributo;
