2f0b1c19bed168f48e47b1a873463383a26e5c8e
jccastrejon <jccastrejon@1581e1ec-bf6e-11de-bbac-836dcc338a7a>
2009-10-31 16:43:40 +0000

    Updated test case

--- trunk/DistributedQueryOptimizer/tst/mx/itesm/ddb/service/AlgebraOptimizerServiceTest.java
+++ trunk/DistributedQueryOptimizer/tst/mx/itesm/ddb/service/AlgebraOptimizerServiceTest.java
@@ -18,15 +18,19 @@ public class AlgebraOptimizerServiceTest extends TestCase {
      */
     public void testBuildOperatorTree() throws ParseException, IOException {
 	Query query;
+	File imageDir;
 	ParserService parserService;
 	AlgebraOptimizerService algebraOptimizerService;
 
+	imageDir = new File("img");
+	imageDir.mkdir();
+	imageDir.deleteOnExit();
 	parserService = new ParserService();
 	algebraOptimizerService = new AlgebraOptimizerService();
 	algebraOptimizerService.setDatabaseDictionaryService(new DatabaseDictionaryService());
 	query = parserService
 		.createQuery("Select * from tabla, tabla2 where tabla.attr = tabla2.attr;");
-	algebraOptimizerService.buildOperatorTree(query, new File("img"));
+	algebraOptimizerService.buildOperatorTree(query, imageDir);
 
 	assertNotNull(query.getOperatorTree());
     }
