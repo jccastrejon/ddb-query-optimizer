09865e2ba9e3390764ec55e5959515cf9696c1b5
jccastrejon <jccastrejon@1581e1ec-bf6e-11de-bbac-836dcc338a7a>
2009-10-31 13:48:55 +0000

    Added Operator Tree and Algebra Optimizer Service

---
+++ DistributedQueryOptimizer/tst/mx/itesm/ddb/service/AlgebraOptimizerServiceTest.java
@@ -0,0 +1,31 @@
+package mx.itesm.ddb.service;
+
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+import mx.itesm.ddb.parser.ParseException;
+
+/**
+ * @author jccastrejon
+ * 
+ */
+public class AlgebraOptimizerServiceTest extends TestCase {
+
+    /**
+     * @throws ParseException
+     * 
+     */
+    public void testBuildOperatorTree() throws ParseException, IOException {
+	Query query;
+	ParserService parserService;
+	AlgebraOptimizerService algebraOptimizerService;
+
+	parserService = new ParserService();
+	algebraOptimizerService = new AlgebraOptimizerService();
+	algebraOptimizerService.setDatabaseDictionaryService(new DatabaseDictionaryService());
+	query = parserService
+		.createQuery("Select * from tabla, tabla2 where tabla.attr = tabla2.attr;");
+	algebraOptimizerService.buildOperatorTree(query, new File("img"));
+    }
+}
