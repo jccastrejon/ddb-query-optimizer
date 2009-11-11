e23a3ef05651c1afe06036942b941335a7bf3895
jccastrejon <jccastrejon@1581e1ec-bf6e-11de-bbac-836dcc338a7a>
2009-11-11 16:50:14 +0000

    Fixed the graphic output of the projection attributes list

--- DistributedQueryOptimizer/src/mx/itesm/ddb/util/RelationalOperator.java
+++ DistributedQueryOptimizer/src/mx/itesm/ddb/util/RelationalOperator.java
@@ -7,8 +7,9 @@ package mx.itesm.ddb.util;
  * 
  */
 public enum RelationalOperator {
-    SELECT("\u03C3", "&#x3C3;"), JOIN("\u22C8", "&#x22C8;"), UNION("\u22C3", "&#x22C3;"), PROJECTION(
-	    "\uu03A0", "&#x3A0;"), PRODUCT("\u00D7", "&#xD7;");
+    SELECT("\u03C3", "&#x3C3;", false), JOIN("\u22C8", "&#x22C8;", false), UNION("\u22C3",
+	    "&#x22C3;", false), PROJECTION("\uu03A0", "&#x3A0;", true), PRODUCT("\u00D7", "&#xD7;",
+	    false);
 
     /**
      * Operator's Unicode code.
@@ -21,16 +22,27 @@ public enum RelationalOperator {
     private String htmlCode;
 
     /**
+     * Whether or not this operator can have multiple expressions in its
+     * definition.
+     */
+    private boolean compoundOperator;
+
+    /**
      * Full constructor that specifies both the unicode and html codes.
      * 
      * @param unicodeCode
      *            Operator's Unicode code.
      * @param htmlCode
      *            Operator's Html code.
+     * @param compoundOperator
+     *            Whether or not this operator can have multiple expressions in
+     *            its definition.
      */
-    private RelationalOperator(final String unicodeCode, final String htmlCode) {
+    private RelationalOperator(final String unicodeCode, final String htmlCode,
+	    final boolean compoundOperator) {
 	this.unicodeCode = unicodeCode;
 	this.htmlCode = htmlCode;
+	this.compoundOperator = compoundOperator;
     }
 
     @Override
@@ -50,5 +62,12 @@ public enum RelationalOperator {
      */
     public String getHtmlCode() {
 	return htmlCode;
+    }
+
+    /**
+     * @return the compoundOperator
+     */
+    public boolean isCompoundOperator() {
+	return compoundOperator;
     }
 }
