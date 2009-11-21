e87af3335d804473c588d1723559597a8d591466
jccastrejon <jccastrejon@1581e1ec-bf6e-11de-bbac-836dcc338a7a>
2009-11-21 15:55:45 +0000

    Fixed each operator implements the isInvalidComparisson() method

--- trunk/DistributedQueryOptimizer/src/mx/itesm/ddb/model/dictionary/PredicateOperator.java
+++ trunk/DistributedQueryOptimizer/src/mx/itesm/ddb/model/dictionary/PredicateOperator.java
@@ -7,8 +7,153 @@ package mx.itesm.ddb.model.dictionary;
  * 
  */
 public enum PredicateOperator {
-    DIFFERENT_THAN("!="), EQUALS_TO("="), GREATER_THAN(">"), LESS_THAN("<"), GREATER_THAN_OR_EQUALS(
-	    ">="), LESS_THAN_OR_EQUALS("<=");
+    DIFFERENT_THAN("!=") {
+	@Override
+	public boolean isInvalidComparisson(final int comparissonResult,
+		final PredicateOperator operator) {
+	    boolean returnValue;
+
+	    returnValue = false;
+	    if (operator == PredicateOperator.EQUALS_TO) {
+		if (comparissonResult == 0) {
+		    returnValue = true;
+		}
+	    }
+
+	    return returnValue;
+	}
+    },
+    EQUALS_TO("=") {
+	@Override
+	public boolean isInvalidComparisson(final int comparissonResult,
+		final PredicateOperator operator) {
+	    boolean returnValue;
+
+	    returnValue = false;
+	    switch (operator) {
+	    case LESS_THAN:
+		if (comparissonResult >= 0) {
+		    returnValue = true;
+		}
+		break;
+	    case LESS_THAN_OR_EQUALS:
+		if (comparissonResult > 0) {
+		    returnValue = true;
+		}
+		break;
+	    case GREATER_THAN:
+		if (comparissonResult <= 0) {
+		    returnValue = true;
+		}
+		break;
+	    case GREATER_THAN_OR_EQUALS:
+		if (comparissonResult < 0) {
+		    returnValue = true;
+		}
+		break;
+	    case EQUALS_TO:
+		if (comparissonResult != 0) {
+		    returnValue = true;
+		}
+		break;
+	    case DIFFERENT_THAN:
+		if (comparissonResult == 0) {
+		    returnValue = true;
+		}
+		break;
+	    }
+
+	    return returnValue;
+	}
+    },
+    GREATER_THAN(">") {
+	@Override
+	public boolean isInvalidComparisson(final int comparissonResult,
+		final PredicateOperator operator) {
+	    boolean returnValue;
+
+	    returnValue = false;
+	    switch (operator) {
+	    case LESS_THAN:
+	    case LESS_THAN_OR_EQUALS:
+	    case EQUALS_TO:
+		if (comparissonResult >= 0) {
+		    returnValue = true;
+		}
+		break;
+	    }
+
+	    return returnValue;
+	}
+    },
+    LESS_THAN("<") {
+	@Override
+	public boolean isInvalidComparisson(final int comparissonResult,
+		final PredicateOperator operator) {
+	    boolean returnValue;
+
+	    returnValue = false;
+	    switch (operator) {
+	    case GREATER_THAN:
+	    case GREATER_THAN_OR_EQUALS:
+	    case EQUALS_TO:
+		if (comparissonResult <= 0) {
+		    returnValue = true;
+		}
+		break;
+	    }
+
+	    return returnValue;
+	}
+    },
+    GREATER_THAN_OR_EQUALS(">=") {
+	@Override
+	public boolean isInvalidComparisson(final int comparissonResult,
+		final PredicateOperator operator) {
+	    boolean returnValue;
+
+	    returnValue = false;
+	    switch (operator) {
+	    case LESS_THAN:
+		if (comparissonResult >= 0) {
+		    returnValue = true;
+		}
+		break;
+	    case LESS_THAN_OR_EQUALS:
+	    case EQUALS_TO:
+		if (comparissonResult > 0) {
+		    returnValue = true;
+		}
+		break;
+	    }
+
+	    return returnValue;
+	}
+    },
+    LESS_THAN_OR_EQUALS("<=") {
+	@Override
+	public boolean isInvalidComparisson(final int comparissonResult,
+		final PredicateOperator operator) {
+	    boolean returnValue;
+
+	    returnValue = false;
+	    switch (operator) {
+	    case GREATER_THAN:
+		if (comparissonResult <= 0) {
+		    returnValue = true;
+		}
+		break;
+	    case GREATER_THAN_OR_EQUALS:
+	    case EQUALS_TO:
+		if (comparissonResult > 0) {
+		    returnValue = true;
+		}
+		break;
+	    }
+
+	    return returnValue;
+	}
+    };
 
     /**
      * Operator description.
@@ -36,46 +181,8 @@ public enum PredicateOperator {
      * @return <em>true</em> if the comparissonValue means that the operators
      *         won't apply for the values compared, <em>false</em> otherwise.
      */
-    public boolean isInvalidComparisson(final int comparissonResult,
-	    final PredicateOperator operator) {
-	boolean returnValue;
-
-	returnValue = false;
-	switch (operator) {
-	case LESS_THAN:
-	    if (comparissonResult >= 0) {
-		returnValue = true;
-	    }
-	    break;
-	case LESS_THAN_OR_EQUALS:
-	    if (comparissonResult > 0) {
-		returnValue = true;
-	    }
-	    break;
-	case GREATER_THAN:
-	    if (comparissonResult <= 0) {
-		returnValue = true;
-	    }
-	    break;
-	case GREATER_THAN_OR_EQUALS:
-	    if (comparissonResult < 0) {
-		returnValue = true;
-	    }
-	    break;
-	case EQUALS_TO:
-	    if (comparissonResult != 0) {
-		returnValue = true;
-	    }
-	    break;
-	case DIFFERENT_THAN:
-	    if (comparissonResult == 0) {
-		returnValue = true;
-	    }
-	    break;
-	}
-
-	return returnValue;
-    }
+    public abstract boolean isInvalidComparisson(final int comparissonResult,
+	    final PredicateOperator operator);
 
     /**
      * Get the Operator's description.
