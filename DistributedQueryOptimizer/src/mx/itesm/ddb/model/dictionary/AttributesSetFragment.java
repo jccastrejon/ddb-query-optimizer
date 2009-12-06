4dd9828f1e492b328590ba6b03cc9a1d7a88fd26
jccastrejon <jccastrejon@1581e1ec-bf6e-11de-bbac-836dcc338a7a>
2009-12-06 02:51:29 +0000

    Fixed, Vertical and Hybrid fragments can have a subset of a global relation's attributes

---
+++ DistributedQueryOptimizer/src/mx/itesm/ddb/model/dictionary/AttributesSetFragment.java
@@ -0,0 +1,11 @@
+package mx.itesm.ddb.model.dictionary;
+
+/**
+ * Identify a fragment that may contain a subset of a Global Relation's
+ * Attributes.
+ * 
+ * @author jccastrejon
+ * 
+ */
+public interface AttributesSetFragment {
+}
